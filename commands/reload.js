a23cf898f79d06ec5837e1abd85dafc5b7c8bd94
Waves <creepykid1101@gmail.com>
2019-04-09 17:30:47 -0500

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -2,6 +2,6 @@ const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
 const cmd = args.shift().toLowerCase();
-delete require.cache[require.resolve(`./commands/${cmd}.js`)]
+delete require.cache[require.resolve(`./${cmd}.js`)]
 message.channel.send(`${cmd} was reloaded.`);
 }
