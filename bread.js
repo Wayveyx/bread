d76e0c2a7c9306150ffcc02d5fdaa1aa5bfecb90
Waves <creepykid1101@gmail.com>
2019-04-08 22:33:36 -0500

    Update bread.js

--- bread.js
+++ bread.js
@@ -11,17 +11,18 @@ bot.user.setActivity("Bread <3", {type: "LISTENING"});
 bot.on("message", async message => {
 let msg = message.content.toLowerCase();
 let args = message.content.slice(pfix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
- try {
- let commandFile = require(`./commands/${command}.js`);
-          commandFile.run(client, message, args);
-} catch (err) {
-bot.channels.get('565011678203215892').send(new Discord.RichEmbed()
+const cmd = args.shift().toLowerCase();
+try { 
+ let commandFile = require(`./commands/${cmd}.js`)
+ commandFile.run(bot, message, args);
+} catch(err) {
+ bot.channels.get('565011678203215892').send(new Discord.RichEmbed()
 .setTitle("Error")
 .setAuthor(message.author.tag)
 .setColor([255, 0, 0])
 .setDescription(err.message)
 .setTimestamp());
+}
 }
 });
 
