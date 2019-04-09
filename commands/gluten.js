0e0df60fb2e9449e24d4db93b431a424b60bdf11
Waves <creepykid1101@gmail.com>
2019-04-08 22:24:42 -0500

    Create gluten.js

---
+++ commands/gluten.js
@@ -0,0 +1,5 @@
+const Discord = requir('discord.js');
+
+exports.run = (bot, message, args) => {
+message.channel.send("Take that shit out of the bread, I don\'t know why you can put it in but not take it out da bread");
+}
