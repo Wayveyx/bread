694631f1feb530761761922650a80988b1117522
Waves <creepykid1101@gmail.com>
2019-04-09 19:13:45 -0500

    Update consume.js

--- commands/consume.js
+++ commands/consume.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 
 exports.run = (bot, message, args) => {
+if(message.author.id !== "452301414689931275") return;
 if(message.channel.name !== "jamerusalem") return;
 message.channel.delete();
 }
