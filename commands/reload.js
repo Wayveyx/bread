9e19b521e0dad74dd7046454a9cd243e157c0a79
Waves <creepykid1101@gmail.com>
2019-04-17 21:56:39 -0500

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
+  if(message.author.id !== "363474941523263518") return;
 const cmd = args.shift().toLowerCase();
 delete require.cache[require.resolve(`./${cmd}.js`)]
 message.channel.send(`${cmd} was reloaded.`);
