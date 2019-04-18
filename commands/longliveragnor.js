2424c8385999ad7e2de8e2f1ab73858eba188bbc
Waves <creepykid1101@gmail.com>
2019-04-17 21:40:00 -0500

    Update longliveragnor.js

--- commands/longliveragnor.js
+++ commands/longliveragnor.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 //i hate this command
 exports.run = async (bot, message, args) => {
+if(message.channel.id !== "468138282279108638") return;
 let createChan = await message.guild.createChannel('jamerusalem');
 message.channel.send("<:check_mark:565315921736892428> Have fun capturing.")
 }
