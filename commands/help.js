9bd9b0504cc739de45f598ec233f6bb04b713880
Waves <creepykid1101@gmail.com>
2019-04-17 22:20:46 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 exports.run = (bot, message, args) => {
 message.channel.send(new Discord.RichEmbed()
 .setTitle('Commands')
-.setDescription('**b!gluten** - Gluten\n**b!longliveragnor** - Do you see Jamerusalem?\n**b!consume** - Consume Jamrusalem.'));
+.setDescription('**b!gluten** - Gluten\n**b!longliveragnor** - Do you see Jamerusalem?'));
 if(message.author.id !== "452301414689931275") return;
 message.channel.send(new Discord.RichEmbed()
 .setTitle('Ragnor Commands')
