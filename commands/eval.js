f27a91db20ca66cc2372319696bc15ee6865aa79
Waves <creepykid1101@gmail.com>
2019-04-19 15:29:38 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -4,24 +4,24 @@ exports.run = (bot, message, args) => {
 if(message.author.id !== "363474941523263518") return;
 	try{
 const code = args;
-				var evaled = eval(code);
-				evaled = functions.clean(evaled);
-				if (typeof evaled !== 'string')
-					evaled = require('util').inspect(evaled);
-				message.channel.send(new Discord.RichEmbed()
-					.setColor([255, 0, 0])
-					.setTitle('Evaled')
-					.setDescription(${clean(evaled)}));
-			} catch (err) {
-				message.channel.send(new Discord.RichEmbed()
-					.setTitle('Error')
-					.setColor([255, 0, 0])
-					.setDescription(`${clean(err)}`));
-			}
+	var evaled = eval(code);
+	evaled = clean(evaled);
+	if (typeof evaled !== 'string')
+	evaled = require('util').inspect(evaled);
+	message.channel.send(new Discord.RichEmbed()
+	.setColor([255, 0, 0])
+    .setTitle('Evaled')
+	.setDescription(${clean(evaled)}));
+	} catch (err) {
+		message.channel.send(new Discord.RichEmbed()
+		.setTitle("Error")
+		.setColor([255, 0, 0])
+		.setDescription(`${clean(err)}`));
+	}
   function clean(text) {
 		if (typeof (text) == 'string')
 			return text
-				.replace(token, 'Sorry, but token is undefined')
+	    		.replace(token, 'Sorry, but token is undefined')
 		else
 			return text;
 	}
