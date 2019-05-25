4ef232476ff6b1f7268f54974add267199342c0a
Waves <creepykid1101@gmail.com>
2019-05-25 03:56:11 -0500

    it works :)

--- commands/announce.js
+++ commands/announce.js
@@ -4,7 +4,7 @@ exports.run = (bot, message, args) => {
 if(!message.content.includes("p-")) return message.channel.send("Please provide a ping type.").then(m => {
                                                                                                 m.delete(5000)});
 message.delete();
-message.channel.send(args.join(" ").slice(args.join().length-3).replace("p-e", "@everyone").replace("p-h", "@here").replace("p-n", " "));
+message.channel.send(args.join(" ").slice(args.join().length-3).replace("p-e", "@everyone").replace("p-h", "@here").replace("p-n", "No Ping"));
 message.channel.send(new Discord.RichEmbed()
 .setTitle("Announcement")
 .setDescription(args.join(" ").replace("p-e", "").replace("p-h", "").replace("p-n", ""))
