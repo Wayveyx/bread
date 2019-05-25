3a6fbf02d6bb936549b21d6ca6df7cdd16664b6f
Waves <creepykid1101@gmail.com>
2019-05-25 03:49:16 -0500

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -4,9 +4,9 @@ exports.run = (bot, message, args) => {
 if(!message.content.includes("p-")) return message.channel.send("Please provide a ping type.").then(m => {
                                                                                                 m.delete(5000)});
 message.delete();
-message.channel.send(args.join(" ").slice(args.length-1).replace("p-e", "@everyone").replace("p-h", "@here").replace("p-n", " "));
+message.channel.send(args.join(" ").slice(args.).replace("p-e", "@everyone").replace("p-h", "@here").replace("p-n", " "));
 message.channel.send(new Discord.RichEmbed()
-.setTitle("Announcement " + args.length)
+.setTitle("Announcement " + args.join().length)
 .setDescription(args.join(" ").replace("p-e", "").replace("p-h", "").replace("p-n", ""))
 .setFooter(`Announced by ${message.author.tag}`));
 }
