a89cf94ebb537434464355bc27a53feda5d8727b
Waves <creepykid1101@gmail.com>
2019-05-24 19:31:44 -0500

    WHY

--- commands/announce.js
+++ commands/announce.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
-if(!message.content.includes("-p")) return message.channel.send("Please provide a ping type.").then(m => {
+if(!message.content.includes("p-")) return message.channel.send("Please provide a ping type.").then(m => {
                                                                                                 m.delete(5000)});
-message.channel.send(args.join(" ").slice(0, "-p").replace(" e", "@everyone").replace(" h", "@here").replace(" n", " "));
+message.channel.send(args.join(" ").slice(0, "p-").replace("e", "@everyone").replace("h", "@here").replace("n", " "));
 message.channel.send(new Discord.RichEmbed()
 .setTitle("Announcement")
-.setDescription(args.join().replace("-p e", "").replace("-p h", "").replace("-p n", ""))
+.setDescription(args.join(" ").replace("p-e", "").replace("p-h", "").replace("p-n", ""))
 .setFooter(`Announced by ${message.author.tag}`));
 }
