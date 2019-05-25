941d7d9149af4be9df51a145998b7ddb8db7e0d8
Waves <creepykid1101@gmail.com>
2019-05-24 19:13:18 -0500

    rip other ver

--- commands/announce.js
+++ commands/announce.js
@@ -1,10 +1,6 @@
 const Discord = require('discord.js');
 
 exports.run = (bot, message, args) => {
-if(!message.content.includes("t-")) return message.channel.send("Please provide a title.").then(m => {
-                                                                                                m.delete(5000)});
-if(!message.content.includes("b-")) return message.channel.send("Please provide a body.").then(m => {
-                                                                                                m.delete(5000)});
 if(!message.content.includes("@")) return message.channel.send("Please provide a ping type.").then(m => {
                                                                                                 m.delete(5000)});
 message.channel.send(new Discord.RichEmbed()
