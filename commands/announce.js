f2e68434e68f6cc52ed722120d3a43bb97de0cda
Waves <creepykid1101@gmail.com>
2019-05-24 19:43:11 -0500

    trying this now lol

--- commands/announce.js
+++ commands/announce.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = (bot, message, args) => {
 if(!message.content.includes("p-")) return message.channel.send("Please provide a ping type.").then(m => {
                                                                                                 m.delete(5000)});
-message.channel.send(args.join(" ").slice(0, "p-").replace("e", "@everyone").replace("p-h", "@here").replace("n", " "));
+message.channel.send(args.join(" ").slice(0, args.length-3).replace("p-e", "@everyone").replace("p-h", "@here").replace("p-n", " "));
 message.channel.send(new Discord.RichEmbed()
 .setTitle("Announcement")
 .setDescription(args.join(" ").replace("p-e", "").replace("p-h", "").replace("p-n", ""))
